fae05b245ad8cd14c709a11353fe73b0f6b165a7
Shihab Hossain <shihabhossain611@gmail.com>
2021-01-23 23:56:37 +0600

    Update Commit

--- backend/src/test/java/com/codewithshihab/backend/UserServiceTest.java
+++ backend/src/test/java/com/codewithshihab/backend/UserServiceTest.java
@@ -1,4 +1,10 @@
 package com.codewithshihab.backend;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class UserServiceTest {
 }
